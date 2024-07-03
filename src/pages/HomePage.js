c0ab4e8b2b10f65022ecac6fd3f808448599c7c4
Peter Huynh <peterhyh@yahoo.com>
2024-07-02 21:38:13 -0500

    player and betting section added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,25 @@ import UserEntrySection from "../components/UserEntrySection";
 const HomePage = () => {
     return (
         <div>
-            <h1>Sports Bet</h1>
+            <h1>Sports Bets</h1>
             <div className="user-entry-section">
-                <UserEntrySection label="Enter Player's Name:" placeholder="Player's name" />
+                <h2>Player Info</h2>
+                <UserEntrySection label="Enter Date:" placeholder="MM/DD/YYYY" />
+                <UserEntrySection label="Enter Player's First Name:" placeholder="First name" />
+                <UserEntrySection label="Enter Player's Last Name:" placeholder="Last name" />
                 <UserEntrySection label="Enter Your Team:" placeholder="Your team" />
                 <UserEntrySection label="Enter Your Opponent Team:" placeholder="Opponent team" />
+                <UserEntrySection label="Sport:" placeholder="NBA/MLB/PGA/etc." />
             </div>
+            <div className="user-entry-section">
+                <h2>Betting Info</h2>
+                <UserEntrySection label="Play:" placeholder="HITS, RUNS, RBI, K, etc." />
+                <UserEntrySection label="More/Less:" placeholder="more/less" />
+                <UserEntrySection label="Betting Number:" placeholder="#" />
+                <UserEntrySection label="Ending Result Number:" placeholder="#" />
+                <UserEntrySection label="Platform:" placeholder="Sleeper/PrizePicks/etc." />
+            </div>
+            <button>Submit</button>
         </div>
     );
 }
