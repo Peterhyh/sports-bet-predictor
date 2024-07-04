df36023dab5599d637074dbac18cff56bf41ae78
Peter Huynh <peterhyh@yahoo.com>
2024-07-04 00:28:11 -0500

    Navbar buttons hover green

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ const NavBar = () => {
     return (
         <div className="navbar">
             <h1>Bet Predictor</h1>
-            <div className="navbar-buttons">
-                <Link to="/">Home</Link>
-                <Link to="/check">Check Player</Link>
+            <div className="navbar-button-section">
+                <Link className="navbar-button" to="/">Home</Link>
+                <Link className="navbar-button" to="/check">Check Player</Link>
             </div>
         </div>
     );
